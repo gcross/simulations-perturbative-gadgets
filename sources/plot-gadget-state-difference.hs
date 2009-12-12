8be374e37afb0a92d5a71542a7939969b9d7c7a7
Gregory Crosswhite <gcross@phys.washington.edu>
2009-12-11 16:46:32 -0800

    Changed plotting script to use the number of sites as the X axis.

--- sources/plot-gadget-state-difference.hs
+++ sources/plot-gadget-state-difference.hs
@@ -33,42 +33,44 @@ import Debug.Trace
 
 dataIteratee :: (MonadIO m) =>
     Int -> Double -> String ->
-    IterAct m (Maybe Int,[(Int,[(Double,String,String)])],Maybe (Double,String),[(Double,String,String)])
+    IterAct m (Maybe Double,[(Double,[(Int,String,String)])],Maybe (Int,String),[(Int,String,String)])
 dataIteratee
     number_of_sites perturbation_strength state_id
     (Nothing,data_set,Nothing,line_set) =
         dataIteratee
             number_of_sites perturbation_strength state_id
-            (Just number_of_sites,data_set,Nothing,line_set)
+            (Just perturbation_strength,data_set,Nothing,line_set)
 dataIteratee
     number_of_sites perturbation_strength state_id
-    (Just previous_number_of_sites,data_set,Just (previous_perturbation_strength,previous_state_id),line_set) =
+    (Just previous_perturbation_strength,data_set,Just (previous_number_of_sites,previous_state_id),line_set) =
         assert (perturbation_strength == previous_perturbation_strength) $
         assert (number_of_sites == previous_number_of_sites) $
-            result' (Just previous_number_of_sites,data_set,Nothing,(perturbation_strength,previous_state_id,state_id):line_set)--'
+            result' (Just previous_perturbation_strength,data_set,Nothing,(number_of_sites,previous_state_id,state_id):line_set)--'
 dataIteratee
     number_of_sites perturbation_strength state_id
-    (Just previous_number_of_sites,data_set,Nothing,line_set) =
-        result' (Just number_of_sites,new_data_set,Just (perturbation_strength,state_id),line_set)--'
-  where
-    new_data_set = if previous_number_of_sites /= number_of_sites
-                        then (previous_number_of_sites,line_set):data_set
-                        else data_set
+    (Just previous_perturbation_strength,data_set,Nothing,line_set)
+  | abs (previous_perturbation_strength - perturbation_strength) > 1e-7
+      = dataIteratee
+            number_of_sites perturbation_strength state_id
+            (Just perturbation_strength,((previous_perturbation_strength,line_set):data_set),Nothing,[])
+  | otherwise
+      = result' (Just perturbation_strength,data_set,Just (number_of_sites,state_id),line_set) -- '
 
 main =
     getArgs
     >>=
-    \(model_id:operator_type:number_of_sites_criteria) ->
+    \(model_id:operator_type:perturbation_strength_criteria) ->
         doIt
             model_id
             (case operator_type of
                 "X" -> pX
                 "Y" -> pY
                 "Z" -> pZ
+                _ -> error $ "Unrecognized operator: " ++ operator_type
             )
             (concatMap
-                (\number_of_sites -> "number_of_sites = " ++ number_of_sites ++ " or ")
-                number_of_sites_criteria
+                (\perturbation_strength -> "abs(perturbation_strength - " ++ perturbation_strength ++ ") < 1e-7 or ")
+                perturbation_strength_criteria
             )
 
 doIt :: String -> SingleQubitOperator -> String -> IO ()
@@ -78,44 +80,44 @@ doIt model_id field_operator filter_string =
     makeConnection "reader"
     >>=
     flip withSession (
-        (fmap (\(Just number_of_sites,data_set,Nothing,line_set) -> ((number_of_sites,line_set):data_set)) $
+        (fmap (\(Just perturbation_strength,data_set,Nothing,line_set) -> ((perturbation_strength,line_set):data_set)) $
             doQuery (sql $ sql_statement) dataIteratee (Nothing,[],Nothing,[])
         )
         >>=
         mapM (
-            \(number_of_sites,line_set) ->
+            \(perturbation_strength,line_set) ->
                 let endpoint_tensor = makeOperatorSiteTensorFromSpecification 1 1 [(1 --> 1) pI]
-                in fmap ((,) number_of_sites) $
+                in fmap ((,) perturbation_strength) $
                     mapM (
-                        \(perturbation_strength,state_id_1,state_id_2) -> do
+                        \(number_of_sites,state_id_1,state_id_2) -> do
                             state_1 <- fmap fromJust . fetchState $ state_id_1
                             state_2 <- fmap fromJust . fetchState $ state_id_2
-                            let number_of_sites = canonicalStateNumberOfSites state_1
-                                expectation = expectationOf $
-                                                    [endpoint_tensor]
-                                                    ++
-                                                    makeMagneticFieldOperatorSiteTensors field_operator (number_of_sites-2)
-                                                    ++
-                                                    [endpoint_tensor]
+                            let expectation =
+                                    expectationOf $
+                                        [endpoint_tensor]
+                                        ++
+                                        makeMagneticFieldOperatorSiteTensors field_operator (canonicalStateNumberOfSites state_1-2)
+                                        ++
+                                        [endpoint_tensor]
                             overlap <- liftIO . evaluate . (realPart . abs) $ (expectation state_1) - (expectation state_2)
-                            return (perturbation_strength,overlap)
+                            return ((fromIntegral number_of_sites :: Double),overlap)
                     ) line_set
         )
     )
     >>=
-    return . map (\(number_of_sites,data_points) ->
-        (PlotStyle LinesPoints (CustomStyle [LineTitle (show number_of_sites ++ " sites"), PointType 2]),data_points)
+    return . map (\(perturbation_strength,data_points) ->
+        (PlotStyle LinesPoints (CustomStyle [LineTitle (show perturbation_strength ++ " sites"), PointType 2]),data_points)
     )
     >>=
     plotPathsStyle
         [Custom "logscale" ["xy"]
         ,Key (Just ["left","top"])
-        ,Title "Difference between Overlaps versus Perturbation Strength"
-        ,XLabel "Perturbation Strength"
-        ,YLabel "Difference between Overlaps "
+        ,Title "Difference between Overlaps versus Number of Sites"
+        ,XLabel "Number of Sites"
+        ,YLabel "Difference between Overlaps"
         ]
   where
     sql_statement :: String
-    sql_statement = "select number_of_sites, perturbation_strength, state_id from gadget_model_simulations natural join solutions where model_id = " ++ model_id ++ " and (" ++ filter_string ++ " False) and level_number <= 1 order by number_of_sites desc, perturbation_strength desc, level_number desc;"
+    sql_statement = "select number_of_sites, perturbation_strength, state_id from gadget_model_simulations natural join solutions where model_id = " ++ model_id ++ " and (" ++ filter_string ++ " False) and level_number <= 1 order by perturbation_strength desc, number_of_sites desc, level_number desc;"
 -- @-node:gcross.20091211100630.1380:@thin plot-gadget-state-difference.hs
 -- @-leo
